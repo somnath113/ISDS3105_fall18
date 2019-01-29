77169a9d8739680aee2e045cf15bc030b92dda72
DarioBoh <bonarettidario@gmail.com>
2019-01-28 23:11:03 -0600

    moved

--- examplePieChart.R
+++
@@ -1,5 +0,0 @@
-ggplot() +
-  geom_col(data =  twitter_users, aes(x = '', y = millions, fill = factor(Year)), width =1) +
-  coord_polar('y', 0)+
-  scale_fill_discrete(name = 'Year') +
-  ggtitle('A plot of twitter users')
