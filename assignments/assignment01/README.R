cf8774a46f787c0c619b99eb2e4510f25d89b234
DarioBoh <bonarettidario@gmail.com>
2019-01-28 23:01:00 -0600

    added assingment

---
+++ assignments/assignment01/README.R
@@ -0,0 +1,39 @@
+#' ---
+#' title: "Assingment 1"
+#' author: ""
+#' ---
+
+
+#' Complete the exercises on a new branch of your git repo. Once you are confident that your solutions are correct, merge the branch you created to your master branch. Follow this workflow:
+#' 1. Save and commit everything up to now
+#' 2. Create a new branch and switch to it `$ git checkout -b solutions` (`$ git checkout [name_of_your_new_branch]` to switch between branches)
+#' 3. Go back to your master `$ git checkout master` and merge it with `solutions` using `$ git merge solutions`
+#' 4. After you merge, commit to master and push to GitHub
+
+
+
+#' Complete to the following tasks and push the responses to your GitHub:
+#' Create a character vector `family` with your family members' names
+#' Create a vector `birth` with their birth years
+#' Create another vector `age` with your family members' ages (calculated using `birth`)
+#' Use `typeof()` to make sure that `family`, `birth`, and `age` are the appropriate type (character, integer/double, integer/double)
+#' Use `names()` to name the elements of `age` with the family members' names from `family`
+#' Use inline code `r ##` to print your name and age (using indexing)
+
+
+
+
+#' Respond to the following questions and push the responses to your GitHub:
+#' What happens when:
+#' You extract using a positive index bigger than the vector length?
+#' You subset by a name that doesn't exist?
+#' You assign a number to position 20 of the age vector? 
+  
+
+#' Carefully read the documentation for `is.vector()`. If `is.vector(x)` retrieves `TRUE`, does it mean that `x` is an atomic vector?
+  
+  
+  
+  
+  
+  
